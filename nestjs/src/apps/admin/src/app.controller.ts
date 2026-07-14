068187b1112901de513d5f9e2f8cf448023c5949
itcvietnam-org-googlemail <itcvietnam.org@googlemail.com>
2026-07-14 11:18:26 +0000

    Turborepo

--- nestjs/src/apps/admin/src/app.controller.ts
+++ nestjs/src/apps/admin/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service.js';
+//import { AppService } from '#admin/app.service';
 
 @Controller()
 export class AppController {
