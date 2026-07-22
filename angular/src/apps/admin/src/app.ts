b866e10c6ebce3e7ac3e67c34353f6cf437c4320
itcvietnam-org-googlemail <itcvietnam.org@googlemail.com>
2026-07-22 04:16:22 +0000

    Docs

--- angular/src/apps/admin/src/app.ts
+++ angular/src/apps/admin/src/app.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Auth } from '@package/auth';
 import { User } from '@package/user';
 
+import {MatButton} from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Auth, User],
+  imports: [RouterOutlet, Auth, User, MatButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
